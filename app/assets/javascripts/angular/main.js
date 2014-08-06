e9b88785ab852184d317b34c3109d890221ac769
Omar Ortega <omar.ortega@tangosource.com>
2014-08-06 10:03:05 -0500

    added angular and angular foundation, support turbolinks

---
+++ app/assets/javascripts/angular/main.js
@@ -0,0 +1,12 @@
+var recruiting = angular.module('recruitingTS', ['mm.foundation']);
+
+angular.bootstrap(document.body, ['recruitingTS']);
+
+recruiting.config(function($httpProvider) {
+  var authToken = angular.element('meta[name="csrf-token"]').attr("content");
+  $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+});
+
+$(document).on('ready page:load', function(){
+  angular.bootstrap(document.body, ['recruitingTS']);
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require angular
+//= require mm-foundation-tpls
+//= require angular/main
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
